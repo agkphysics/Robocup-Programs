82db4d28631d2a57ce2b810571f08c6fe7fd3f9c
Daniel Britten <brittendan@gmail.com>
2013-08-16 11:42:12 +1200

    Tidyied up and deleted non-motor related stuff
    Deleted all line-following and ultrasonic sensor related lines.
    Deleted all manual control of stepper motors
    Tried to cut down on extra lines of code where possible
    Re-ordered variables and where functions are defined to be ready to add
    pilot functions: pilotForward(); pilotBackward(); pilotRotateRight();
    pilotRotateLeft(); pilotSwingRight(); pilotSwingLeft();
    Use of left and right in these future functions is primarily to increase
    readability of code. (as opposed to having to work out whether
    rotate(-40); means left or right each time.
    Note that this will still be easy to change in the future by writing new
    functions rotate() and swing() that use rotateRight and rotateLeft.

--- Testing_Motors/Testing_Motors.ino
+++ Testing_Motors/Testing_Motors.ino
@@ -1,98 +1,24 @@
 #include <Scheduler.h>
-
-/* pinconsts.h - Pin constants for the robot */
-
-/* IR LED Array */
-#define lineFollowingPin_1 7
-#define lineFollowingPin_2 6
-#define lineFollowingPin_3 5
-#define lineFollowingPin_4 4
-#define lineFollowingPin_5 3
-#define lineFollowingPin_6 2
-#define lineFollowingPin_7 1
-#define lineFollowingPin_8 0
-
-/* Ultrasonic Sensor */
-#define ultrasonicPinTrigger 12
-#define ultrasonicPinEcho 13
-
-/* Motors */
-#define motorLeftDirPin 6
-#define motorLeftStepPin 7
-#define motorRightDirPin 4
-#define motorRightStepPin 5
-
-#define liftArmUpPin 2
-#define liftArmClosePin 3
-
-/* Switches */
-//#define switchSuchAndSuch 1
-
-
-
 #include <AccelStepper.h>
 
-AccelStepper leftMotor(AccelStepper::DRIVER, motorLeftStepPin, motorLeftDirPin);
-AccelStepper rightMotor(AccelStepper::DRIVER, motorRightStepPin, motorRightDirPin);
+AccelStepper leftMotor(AccelStepper::DRIVER, 7, 6);
+AccelStepper rightMotor(AccelStepper::DRIVER, 5, 4);
 
-#define MOTOR_LEFT 0
-#define MOTOR_RIGHT 1
 
-void setupMotors()
-{
-	pinMode(motorLeftDirPin, OUTPUT);
-	pinMode(motorLeftStepPin, OUTPUT);
-	pinMode(motorRightDirPin, OUTPUT);
-	pinMode(motorRightStepPin, OUTPUT);
-        
-        pinMode(liftArmUpPin, OUTPUT);
-        pinMode(liftArmClosePin, OUTPUT);
-}
+int leftSpeed = 500; //As would be calculated by linefollowing procedure
+int rightSpeed = -500; //As would be calculated by linefollowing procedure
+boolean runLeftMotor = true;
+boolean runRightMotor = true;
 
 double distanceToSteps(double dist)
 {
   return (dist * 100) / (30.25 * 3.14159265358979);
 }
 
-void manualMove(int motor, double dist)
-{
-  int steps = (int)distanceToSteps(dist);
-  int pin = (motor == MOTOR_LEFT) ? motorLeftStepPin : motorRightStepPin;
-  digitalWrite((motor == MOTOR_LEFT) ? motorLeftDirPin : motorRightDirPin, (dist < 0) ? LOW : HIGH);
-
-  int i;
-  for (i = 0; i < steps * 8; i++)
-  {
-    digitalWrite(pin, LOW);
-    digitalWrite(pin, HIGH);
-    delayMicroseconds(750);
-  }
-}
-
-
-
-
-#include <QTRSensors.h>
-
-QTRSensorsAnalog lf((unsigned char[]){lineFollowingPin_1,lineFollowingPin_2,lineFollowingPin_3,lineFollowingPin_4,lineFollowingPin_5,lineFollowingPin_6,lineFollowingPin_7,lineFollowingPin_8}, 8);
-
-
-
-
-void lineFollowing()
-{
-  
-}
-
-  int leftSpeed = 500; //As would be calculated by linefollowing procedure
-  int rightSpeed = -500; //As would be calculated by linefollowing procedure
-
 void setup()
 {
-  setupMotors();
-  pinMode(13, OUTPUT); // For the led
-  
-  digitalWrite(13, HIGH);
+  pinMode(13, OUTPUT); // For the led 
+  digitalWrite(13, HIGH); //Flashing LED once, could be removed later
   delay(500);
   digitalWrite(13, LOW);
   leftMotor.setMaxSpeed(600);
@@ -100,21 +26,17 @@ void setup()
   leftMotor.setAcceleration(10000);
   rightMotor.setAcceleration(10000);
 
-  leftMotor.setSpeed(leftSpeed);
-  rightMotor.setSpeed(rightSpeed);
+  leftMotor.setSpeed(leftSpeed);//Must be set once to allow movement, setSpeed(float) should be called after moveTo, not needed to be called after move()
+  rightMotor.setSpeed(rightSpeed);//Must be set once to allow movement
 
-  leftMotor.moveTo(-500);
-  rightMotor.moveTo(500);
-
-//  manualMove(MOTOR_RIGHT, 500);
+  leftMotor.move(-500);
+  rightMotor.move(500);
  
- Scheduler.startLoop(loop2);
+ Scheduler.startLoop(runMotorLoop);
 
  
 }
 
-boolean useLeftMotor = true;
-boolean useRightMotor = true;
 
 void loop()
 {
@@ -125,18 +47,18 @@ void loop()
   digitalWrite(13, LOW);
 }
 
-void loop2() {
-  if (useLeftMotor == true) {
-     leftMotor.run();
+void runMotorLoop() {
+  if (runLeftMotor == true) {
+    leftMotor.run();
     if (leftMotor.distanceToGo () == 0) {
-      useLeftMotor==false;
+      runLeftMotor==false;
     }
   }
   
-    if (useRightMotor == true) {
-     rightMotor.run();
+    if (runRightMotor == true) {
+      rightMotor.run();
       if (rightMotor.distanceToGo () == 0) {
-        useRightMotor==false;
+        runRightMotor==false;
       }
     }
   
