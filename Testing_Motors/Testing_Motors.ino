efe19633b638bb397068063e069c104a14a153a2
Daniel Britten <brittendan@gmail.com>
2013-08-16 16:39:25 +1200

    fixed problems from using long + added swing()

--- Testing_Motors/Testing_Motors.ino
+++ Testing_Motors/Testing_Motors.ino
@@ -5,15 +5,15 @@ AccelStepper leftMotor(AccelStepper::DRIVER, 7, 6);
 AccelStepper rightMotor(AccelStepper::DRIVER, 5, 4);
 
 
-int leftSpeed = 500; //Initial Speed (for testing purposes)
-int rightSpeed = -500; //Initial Speed (for testing purposes)
+int leftSpeed = 800; //Initial Speed (for testing purposes)
+int rightSpeed = 800; //Initial Speed (for testing purposes)
 boolean runLeftMotor = true;
 boolean runRightMotor = true;
 
 //Definition of Movement related functions
-long cmToSteps(long cm)
+long cmToSteps(float cm)
 {
-  return (cm * 1600)/(6.46 * 3.14159265358979);
+  return (long)((cm * 1600.0)/(6.46 * 3.14159265358979));
 }
 
 boolean motorsRunning() {
@@ -25,19 +25,25 @@ boolean motorsRunning() {
   }
 }
 
-void straight(long cm) {
+void straight(float cm) {
   leftMotor.move(cmToSteps(cm));
   rightMotor.move(-cmToSteps(cm));
 }
 
-void rotate(long deg) {//positive rotates Right, negative rotates Left
- // long rotationCm = ((deg/360)*3.14159265358979*13.25);
-  leftMotor.move(cmToSteps((deg/360)*3.14159265358979*13.7));
-  rightMotor.move(cmToSteps((deg/360)*3.14159265358979*13.7));
+void rotate(float deg) {//positive rotates Right, negative rotates Left
+  leftMotor.move(cmToSteps((deg/360.0)*3.14159265358979*13.99)); //Note that 13.99 is the 'diameter' that gives the most accurate movement for a 360 rotate
+  rightMotor.move(cmToSteps((deg/360.0)*3.14159265358979*13.99));
 }
 
+void swingWithRight (float deg) { //Positive turns LEFT!!!  Negative turns RIGHT!!! (going backwards)
+  leftMotor.move(0);
+  rightMotor.move(-cmToSteps((deg/360.0)*3.14159265358979*13.99*2));
+}
 
-
+void swingWithLeft(float deg) {//Positive turns Right, Negative turns Left (going backwards)
+  rightMotor.move(0);
+  leftMotor.move(cmToSteps((deg/360.0)*3.14159265358979*13.99*2));
+}
 
 void setup()
 {
@@ -45,8 +51,8 @@ void setup()
   digitalWrite(13, HIGH); //Flashing LED once, could be removed later
   delay(500);
   digitalWrite(13, LOW);
-  leftMotor.setMaxSpeed(600);
-  rightMotor.setMaxSpeed(600);
+  leftMotor.setMaxSpeed(900);
+  rightMotor.setMaxSpeed(900);
   leftMotor.setAcceleration(10000);
   rightMotor.setAcceleration(10000);
 
@@ -72,11 +78,14 @@ void loop()
   digitalWrite(13, LOW);
   rotate(360);
   while(motorsRunning()) yield();
+  straight(10);
+  while(motorsRunning()) yield();
+  swingWithLeft(90.0);
+  while(motorsRunning()) yield();
+  swingWithRight(90.0);
+  while(motorsRunning()) yield();
   delay(1000);
 }
-
-
-
 
 void runMotorLoop() {
   leftMotor.run();
