2a36f2891931cee76566b2d936b92f242d66c819
Daniel Britten <brittendan@gmail.com>
2013-08-22 14:02:42 +1200

    Rotates to North

--- Testing_Compass_Sensor/Testing_Compass_Sensor.ino
+++ Testing_Compass_Sensor/Testing_Compass_Sensor.ino
@@ -1,16 +1,32 @@
 #include <Compass.h>
 #include <Wire.h>
+#include <Motors.h>
+#include <AccelStepper.h>
+#include <Scheduler.h>
 
 Compass compass;
+AccelStepper leftMotor(AccelStepper::DRIVER, 7, 6);
+AccelStepper rightMotor(AccelStepper::DRIVER, 5, 4);
+Motors motors(leftMotor, rightMotor);
 
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
+  Scheduler.startLoop(runMotorLoop);
+  motors.setup();
 }
 
 void loop(){
   Serial.print(compass.heading());
   Serial.println(" heading");
   delay(100);
+  motors.rotate(-compass.heading());
+  delay(5000);
+}
+
+
+void runMotorLoop() {
+  motors.run();
+  yield();
 }
