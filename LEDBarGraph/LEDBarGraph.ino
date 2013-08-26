872d980e06a5433b2db3d171c270268d5afb3add
Aaron Keesing <agkphysics@gmail.com>
2013-08-26 17:56:11 +1200

    Minor error in LEDBarGraph

--- LEDBarGraph/LEDBarGraph.ino
+++ LEDBarGraph/LEDBarGraph.ino
@@ -13,7 +13,7 @@ void turnOnLed(int led, boolean exclusive)
   digitalWrite(ledBarGraphPins[led], HIGH);
   if (exclusive)
   {
-    for (int i = 0; i < 10 && i += led; i++)
+    for (int i = 0; i < 10 && i != led; i++)
     {
       digitalWrite(ledBarGraphPins[i], LOW);
     }
