56cb9a78406bc33b3236b8672193950016c0658c
Aaron Keesing <agkphysics@gmail.com>
2013-08-30 17:27:17 +1200

    EndTile working almost perfectly.

--- Testing_EndTile/Testing_EndTile.ino
+++ Testing_EndTile/Testing_EndTile.ino
@@ -17,56 +17,115 @@ float initialHeading;
 Motors motors(leftMotor, rightMotor);
 Servo closeArm;
 
-void endTile()
+void endTile(boolean left)
 {
   initialHeading = compass.heading();
   
   motors.setSpeeds(500, 500);
   motors.straight(0.0);
-  motors.rotate(-90.0);
+  
+  if (left) motors.rotate(-90.0);
   motors.wait();
   
-  motors.setSpeeds(50, 50);
-  motors.rotate(180.0);
-  float dist = ultrasonic.distance();
+  motors.setSpeeds(100, 100);
+  
+  motors.rotate(90.0);
+  float dist = 60.0;
   float headingToCan = 0.0;
-  while (motors.running())
+  while (true)
   {
-    float currentDist = ultrasonic.distance();
-    if (currentDist < dist)
+    /* Search for can */
+    while (motors.running())
     {
-      headingToCan = compass.heading();
-      dist = currentDist;
+      float currentDist = ultrasonic.distance();
+      if (currentDist < dist)
+      {
+        headingToCan = compass.heading();
+        dist = currentDist;
+      }
+      yield();
     }
-    yield();
+    if (dist <= 50.0) break;
+    else
+    {
+      dist = 60.0;
+      headingToCan = 0.0;
+      
+      motors.setSpeeds(50, 50);
+      motors.rotate(-90);
+      motors.wait();
+      motors.rotate(90.0);
+    }
   }
   float finishHeading = compass.heading();
   
   motors.setSpeeds(500, 500);
-  if (finishHeading < headingToCan) motors.rotate(-(finishHeading + 360.0 - headingToCan));
-  else motors.rotate(-(finishHeading - headingToCan));
+
+  while (abs(compass.heading() - headingToCan) > 2.0)
+  {
+    motors.rotate(-1.0);
+    motors.wait();
+  }
+  
+  /* An extra few degrees just in case... */
+  if (dist >= 40.0) motors.rotate(-13.0);
+  else motors.rotate(-8.0);
   motors.wait();
   
-  float distToCan = ultrasonic.distance();
-  motors.rotate(-15.0);
-  motors.wait();
+  float distToCan = dist;
   motors.straight(distToCan);
+  motors.wait();
+  
+  /* Swing to the right in case the can is too far right */
+  if (dist >= 40.0) motors.swingWithLeft(30.0);
+  else motors.swingWithLeft(45.0);
   motors.wait();
   delay(250);
   
-  closeArm.write(90);
-  delay(1000);
+  closeArm.write(0); // MUST be 0, not 180!
+  delay(750);
   
-  /* Lift arm etc... */
+  /* Lift arm */
+  digitalWrite(2, HIGH);
+  delay(2000);
+  digitalWrite(2, LOW);
   
+  if (dist >= 40.0) motors.swingWithLeft(-30.0);
+  else motors.swingWithLeft(-45.0);
+  motors.wait();
   motors.straight(-distToCan);
   motors.wait();
-  if (headingToCan < initialHeading) motors.rotate(initialHeading - headingToCan);
-  else motors.rotate(initialHeading + 360.0 - headingToCan);
+  if (left) motors.rotate(-10.0);
+  else motors.rotate(10.0);
+  
+  if (left)
+  {
+    while (abs(compass.heading() - initialHeading) > 2.0)
+    {
+      motors.rotate(1.0);
+      motors.wait();
+    }
+  }
+  else
+  {
+    while (abs(compass.heading() - initialHeading) > 2.0)
+    {
+      motors.rotate(-1.0);
+      motors.wait();
+    }
+  }
+  
+  motors.straight(47.0);
+  motors.wait();
+  
+  /* Drop can etc... */
+  closeArm.write(45);
+  delay(400);
+  motors.swingWithLeft(-90.0);
   motors.wait();
   
   digitalWrite(13, HIGH);
-  delay(1000);
+  while (true) {}
 }
 
 void setup()
@@ -76,7 +135,7 @@ void setup()
   
   Wire.begin();
   motors.setup();
-  closeArm.attach(www);
+  closeArm.attach(3);
   Scheduler.startLoop(runMotors);
 
   /* Just to indicate the program is about to start */
@@ -88,7 +147,7 @@ void setup()
 
 void loop()
 {
-  endTile();
+  endTile(false);
 }
 
 void runMotors()
