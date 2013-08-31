3adaa0ec556b6f03cc44ebd61aa296c09cfc35ed
Aaron Keesing <agkphysics@gmail.com>
2013-08-31 20:43:46 +1200

    LEDBarGraph now working with 7 LEDs.

--- LEDBarGraph/LEDBarGraph.ino
+++ LEDBarGraph/LEDBarGraph.ino
@@ -1,26 +1,53 @@
-const int ledBarGraphPins[10] = {};
+const int ledBarGraphPins[7] = {31,33,35,37,39,41,43};
 
 void setupLEDBarGraph()
 {
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < 7; i++)
   {
     pinMode(ledBarGraphPins[i], OUTPUT);
   }
 }
 
-void turnOnLed(int led, boolean exclusive)
+void turnOnLED(int led, boolean exclusive)
 {
   digitalWrite(ledBarGraphPins[led], HIGH);
   if (exclusive)
   {
-    for (int i = 0; i < 10 && i != led; i++)
+    for (int i = 0; i < 7 && i != led; i++)
     {
       digitalWrite(ledBarGraphPins[i], LOW);
     }
   }
 }
 
-void turnOffLed(int led)
+void turnOffLED(int led)
 {
   digitalWrite(ledBarGraphPins[led], LOW);
 }
+
+void setup()
+{
+  setupLEDBarGraph();
+}
+
+void loop()
+{
+  for (int i = 0; i < 7; i++)
+  {
+    turnOnLED(i, true);
+    delay(250);
+  }
+  turnOffLED(6);
+  
+  for (int i = 0; i < 7; i++)
+  {
+    turnOnLED(i, false);
+    delay(250);
+  }
+  for (int i = 0; i < 7; i++)
+  {
+    turnOffLED(i);
+    delay(250);
+  }
+}
+
