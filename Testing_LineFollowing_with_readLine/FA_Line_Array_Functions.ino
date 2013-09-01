f5f9238824d6ec82ada1066a72fe877b6dbe5698
Daniel Britten <brittendan@gmail.com>
2013-09-01 16:26:10 +1200

    Moved calibratelinefollowing to setup functions
    Now moves itself to calibrate

--- Testing_LineFollowing_with_readLine/FA_Line_Array_Functions.ino
+++ Testing_LineFollowing_with_readLine/FA_Line_Array_Functions.ino
@@ -12,8 +12,7 @@ void setLineFollowingSpeeds()
       leftSpeed = leftSpeedFactor;
       rightSpeed = rightSpeedFactor * (1.0 - 15.0 * currentLinePosition* currentLinePosition*currentLinePosition);
    }
-
-
+   
   motors.setActiveSpeeds(leftSpeed, rightSpeed);
 }
 
@@ -29,20 +28,6 @@ boolean reachedEndTile() { //TODO
   return false;
 }
 
-void calibrateLineFollowing()
-{
-  delay(500);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);    // turn on Arduino's LED to indicate we are in calibration mode
-  motors.rotate(10000);
-  for (int i = 0; i < 400; i++)  // make the calibration take about 10 seconds
-  {
-    qtra.calibrate();       // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
-    yield();
-  }
-  motors.setSpeeds(0.0,0.0);
-  digitalWrite(13, LOW);     // turn off Arduino's LED to indicate we are through with calibration
-}
 
 float blackness() { //0 to 8000
   //Must have just called this in LineFollowingSetSpeeds: qtra.readCalibrated(currentSensorValues);
--- Testing_LineFollowing_with_readLine/FB_Setup_Functions.ino
+++ Testing_LineFollowing_with_readLine/FB_Setup_Functions.ino
@@ -1,7 +1,25 @@
 void setupRobot(){
-  calibrateLineFollowing();
   Wire.begin();
   Scheduler.startLoop(runMotorLoop);
   motors.setup();
   Serial.begin(9600);
+ calibrateLineFollowing();
+}
+
+
+void calibrateLineFollowing()
+{
+  delay(500);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);    // turn on Arduino's LED to indicate we are in calibration mode
+  motors.setMaxSpeeds(300.0, 300.0);
+  motors.rotate(360*2);
+  for (int i = 0; i < 400; i++)  // make the calibration take about 10 seconds
+  {
+    qtra.calibrate();       // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
+    yield();
+  }
+  digitalWrite(13, LOW);     // turn off Arduino's LED to indicate we are through with calibration
+  motors.wait();
+  motors.setMaxSpeeds(500, 500);
 }
