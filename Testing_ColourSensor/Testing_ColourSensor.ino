305a3d1706463bee1632f7504435f279b9072b85
Daniel Britten <brittendan@gmail.com>
2013-09-02 22:04:55 +1200

    Detecting Green reliably

--- Testing_ColourSensor/Testing_ColourSensor.ino
+++ Testing_ColourSensor/Testing_ColourSensor.ino
@@ -12,7 +12,65 @@ void setup()
   pinMode(10, OUTPUT);
 
   Serial.begin(9600);
+  calibrateSensors();
+}
+
+void loop()
+{
+  float redL, greenL, blueL;
+  float redR, greenR, blueR;
   
+  redL = left.getRed();
+  delay(10);
+  greenL = left.getGreen();
+  delay(10);
+  blueL = left.getBlue();
+  delay(10);
+  
+  redR = right.getRed();
+  delay(10);
+  greenR = right.getGreen();
+  delay(10);
+  blueR = right.getBlue();
+  delay(10);
+    
+  Serial.print("Left values: (");
+  Serial.print(redL);
+  Serial.print(", ");
+  Serial.print(greenL);
+  Serial.print(", ");
+  Serial.print(blueL);
+  Serial.print(")\t");
+  
+  Serial.print("Right values: (");
+  Serial.print(redR);
+  Serial.print(", ");
+  Serial.print(greenR);
+  Serial.print(", ");
+  Serial.print(blueR);
+  Serial.print(")");
+  
+  if (((greenL - redL) > 15) && (greenL/(greenL+redL) > 0.7)) {
+    digitalWrite(6, HIGH);
+     Serial.print("   LEFT GREEN!!!   ");
+  }
+  else digitalWrite(6, LOW);
+  
+  
+  if (((greenR - redR) > 15) && (greenR/(greenR+redR) > 0.7)) {
+    digitalWrite(7, HIGH);
+     Serial.print("   RIGHT GREEN!!!   ");
+  }
+  else digitalWrite(7, LOW);
+  
+  
+  Serial.println();
+  delay(140);
+}
+
+void calibrateSensors(){
+  while(!Serial.available() && Serial.read() != 'y');
+  Serial.read(); // just read the 'enter'  
   Serial.println("Adjust left white color, show something white to the sensor and press y."); 
   while(!Serial.available() && Serial.read() != 'y');
   Serial.read(); // just read the 'enter'
@@ -64,84 +122,3 @@ void setup()
   }
   Serial.println();
 }
-
-void loop()
-{
-  int redL, greenL, blueL;
-  int redR, greenR, blueR;
-  
-  redL = left.getRed();
-  delay(50);
-  greenL = left.getGreen();
-  delay(50);
-  blueL = left.getBlue();
-  delay(50);
-  
-  redR = right.getRed();
-  delay(50);
-  greenR = right.getGreen();
-  delay(50);
-  blueR = right.getBlue();
-  delay(50);
-  
-  /*
-  unsigned char *values = left.getRawValues();
-  
-  Serial.print("Values: (");
-  for (int i = 0; i < 3; i++)
-  {
-    Serial.print(values[i]);
-    Serial.print(", ");
-  }
-  Serial.print(")");
-  */
-  
-  Serial.print("Left values: (");
-  Serial.print(redL);
-  Serial.print(", ");
-  Serial.print(greenL);
-  Serial.print(", ");
-  Serial.print(blueL);
-  Serial.print(")\t");
-  
-  Serial.print("Right values: (");
-  Serial.print(redR);
-  Serial.print(", ");
-  Serial.print(greenR);
-  Serial.print(", ");
-  Serial.print(blueR);
-  Serial.print(")");
-  
-  
-  /* Method 1 */
-  if (abs(redL - 15) < 7 && abs(greenL - 40) < 7 && abs(blueL - 20) < 7) 
-  {
-    digitalWrite(6, HIGH);
-    digitalWrite(13, HIGH);
-  }
-  else
-  {
-    digitalWrite(6, LOW);
-    digitalWrite(13, LOW);
-  }
-  
-  if (abs(redR - 15) < 7 && abs(greenR - 40) < 7 && abs(blueR - 20) < 7)
-  {
-    digitalWrite(7, HIGH);
-    digitalWrite(13, HIGH);
-  }
-  else
-  {
-    digitalWrite(7, LOW);
-    digitalWrite(13, LOW);
-  }
-  
-  
-  /* Method 2 */
-  if (greenL > (redL + blueL) / 2) digitalWrite(6, HIGH);
-  else digitalWrite(6, LOW);
-  
-  Serial.println();
-  delay(700);
-}
-
