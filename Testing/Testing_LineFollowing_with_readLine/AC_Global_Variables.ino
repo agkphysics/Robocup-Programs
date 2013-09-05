1576c54fce09ee8624b66f40630d9f3cc0adc107
Aaron Keesing <agkphysics@gmail.com>
2013-09-05 16:44:24 +1200

    Put in preprogrammed square tile.

--- Testing/Testing_LineFollowing_with_readLine/AC_Global_Variables.ino
+++ Testing/Testing_LineFollowing_with_readLine/AC_Global_Variables.ino
@@ -9,9 +9,12 @@ float rightSpeed = 600.0; //Initial Speed (for testing purposes)
 float leftSpeedFactor = 600.0; //Max Speed on straight for linefollowing
 float rightSpeedFactor = 600.0; //Max Speed on straight for linefollowing
 
+
+float maxSpeedManualSections = 4000.0;
+
 int gridlockIntersection = 1;
 
 boolean reachedEndTile = false;
 
-int mapArray[] = {GRIDLOCK};
+int mapArray[] = {SQUARE};
 int maxArrayIndex = (sizeof(mapArray)/sizeof(int)) - 1;
