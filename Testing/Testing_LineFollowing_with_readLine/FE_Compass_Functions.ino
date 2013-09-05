cf5a1bc709cab26696e33efdb987409cfd9bce4d
Legitbacon <Mattster9000@gmail.com>
2013-09-05 21:55:14 +1200

    Implemented align() with working compass.correctedHeading()
    All works :)

--- Testing/Testing_LineFollowing_with_readLine/FE_Compass_Functions.ino
+++ Testing/Testing_LineFollowing_with_readLine/FE_Compass_Functions.ino
@@ -5,14 +5,18 @@ void align() {
   motors.wait();
 }
 
+	 
 float degreesToNearest90() {
-    int currentHeading = (int)(compass.heading());
+    int currentHeading = (int)(compass.correctedHeading());
 	
     int currentRelativeHeading = currentHeading - initialHeading;
     if (currentRelativeHeading < 0) {
       currentRelativeHeading = currentRelativeHeading + 360;
     }
-	
+    Serial.print("CurrentRelativeHeading: ");
+    Serial.println(currentRelativeHeading);
     int closest90 = ((currentRelativeHeading + 45)/90)*90;
+    Serial.print("closest90: ");
+    Serial.println(closest90);
     return (float)(closest90 - currentRelativeHeading);
 }
--- Testing/Testing_LineFollowing_with_readLine/ZB_Testing_Align.ino
+++
@@ -1,7 +0,0 @@
-/*void setup() {
-    setupRobot(); //Includes calibrate line sensors
-}
-
-void loop() {
-  while(digitalRead(PIN_TOWER_SWITCH) == LOW);
-  align();*/
